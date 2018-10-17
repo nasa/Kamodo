d5bbcf36f372f3a4cf34f6e7c04fa166f6208a05
asherp <apembroke@gmail.com>
2018-10-17 15:03:03 -0400

    setup modified

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from distutils.core import setup
 setup(
     name='kamodo',
     version='0.1dev',
+    author='Asher Pembroke',
+    url='https://ccmc.gsfc.nasa.gov/',
+    author_email='apembroke@gmail.com',
     packages=['kamodo',],
     license='Apache2.0',
     long_description=open('README.md').read(),
