399b1f2cc77502149828620720ebfd4dfec3eab7
rebeccaringuette <49281118+rebeccaringuette@users.noreply.github.com>
2022-03-23 18:35:53 -0400

    Add files via upload
    Adding/improving docstrings.

--- kamodo_ccmc/flythrough/model_wrapper.py
+++ kamodo_ccmc/flythrough/model_wrapper.py
@@ -53,7 +53,15 @@ model_dict = {0:'CTIPe', 1:'GITM', 2:'IRI', 3:'SWMF_IE', 4:'TIEGCM', 5:'OpenGGCM
               6:'AmGEO'}
 
 def convert_model_string(model_int):
-    '''Converts numerical model reference to string.'''
+    '''Converts numerical model reference to string.
+    
+    Input:
+        model_int: An integer or string representing the model.
+    Output:
+        If model_int is an integer, the string corresponding to that model is 
+            returned. If model_int is not an integer, model_int is returned 
+            unchanged.
+    '''
     
     if isinstance(model_int,int): 
         return model_dict[model_int]
@@ -62,7 +70,14 @@ def convert_model_string(model_int):
 
 
 def Choose_Model(model):
-    '''Returns module specific to the model requested.'''
+    '''Returns module specific to the model requested.
+    
+    Input:
+        model: A string or integer corresponding to the desired model. If the
+            string is empty, print statements are executed.
+    Output: The module associated with the model reader for the desired model. 
+        If model is an empty string, None is returned.
+    '''
     #UPDATE THIS AS MORE MODELS ARE ADDED
 
     if model == '':  #Give a list of possible values
@@ -106,8 +121,17 @@ def Choose_Model(model):
 
 def FileSearch(model, file_dir, call_type='normal'):
     '''Returns list of model data files for each model based on the name pattern.
-    If only one file per day, or reader knows of the different filenames, then return string.
-    Else, return an array of filename patterns.'''
+    
+    Inputs:
+        model: A string or integer corresponding to the desired model. 
+        file_dir: A string indicated the full file path to the directory desired.
+        call_type: A string (default='normal'). If call_type is 'normal', the
+            normal time range is used in the file search logic, typically one day. 
+            If call_type is another string, then a smaller time range is used, 
+            typically one hour.
+    Outputs: If the model output only produces one file per day, or the reader 
+        knows of the different filenames, then a string is returned. Otherwise,
+        an array of filename patterns is returned.'''
     #UPDATE THIS AS NEW MODELS ARE ADDED
     
     if isinstance(model,int): model = model_dict[model]  #convert to string
@@ -159,14 +183,26 @@ def FileSearch(model, file_dir, call_type='normal'):
         
 
 def Model_Reader(model):
-    '''Returns model reader for requested model. Model agnostic.'''
+    '''Returns model reader for requested model. Model agnostic.
+    Input: model: A string or integer associated with the desired model.
+    
+    Output: The MODEL Kamodo class object in the desired model reader.
+    '''
     
     module = Choose_Model(model)
     return module.MODEL()  #imports Kamodo
         
 
 def Model_Variables(model, return_dict=False):
-    '''Returns model variables for requested model. Model agnostic.'''
+    '''Returns model variables for requested model. Model agnostic.
+    
+    Inputs:
+        model: A string or integer associated with the desired model.
+        return_dict: A boolean (default=False). If False, the full variable dictionary
+            associated with the model is printed to the screen and None is returned. 
+            If True, the full variable dictionary is returned without any printed statements.
+        Output: None or the variable dictionary. (See return_dict description.)
+    '''
     
     #choose the model-specific function to retrieve the variables
     module = Choose_Model(model)
@@ -185,7 +221,17 @@ def Model_Variables(model, return_dict=False):
         return    
     
 def File_Variables(model, file_dir, return_dict=False):
-    '''Print list of variables in model data output stored in file_dir.'''
+    '''Print list of variables in model data output stored in file_dir.
+    
+    Inputs:
+        model: A string or integer associated with the desired model.
+        file_dir: A string giving the full file path for the chosen directory.
+        return_dict: A boolean (default=False). If False, the variable dictionary
+            associated with the files in the given directory is printed to the screen 
+            and None is returned. If True, the same variable dictionary is returned 
+            without any printed statements.
+        Output: None or the variable dictionary. (See return_dict description.)   
+    '''
 
     reader = Model_Reader(model)
     file_patterns = FileSearch(model, file_dir)
@@ -219,9 +265,26 @@ def File_Variables(model, file_dir, return_dict=False):
             print()
         return
     
-def File_Times(model, file_dir, output=True):
+def File_Times(model, file_dir, print_output=True):
     '''Return/print time ranges available in the data in the given dir. Also
-    performs file conversions of new data if needed.'''
+    performs file conversions if the expected converted files are not found for
+    the files or file naming patterns detected. If the model_times.csv file is 
+    not found in the given directory, it is created. If new files or file naming 
+    patterns are detected that are not in the model_times.csv file in the given 
+    directory, the file is updated.
+    
+    Inputs:
+        model: A string or integer associated with the desired model.
+        file_dir: A string giving the full file path for the chosen directory.
+        print_output: A boolean (default=True). If True, the time information associated
+            with each file or file naming pattern is printed to the screen. If
+            False, no print statements are executed.
+    Output: A dictionary containing a date string as the keys and a list of the 
+        time information for each file name or file naming pattern. The list 
+        contains the the file name or file naming pattern, the beginning and 
+        end times in UTC in string and timestamp format, followed by the smallest 
+        time resolution in seconds detected in the data.
+    '''
     
     #get time ranges from data
     from kamodo_ccmc.flythrough.SF_utilities import check_timescsv
@@ -229,7 +292,7 @@ def File_Times(model, file_dir, output=True):
     times_dict = check_timescsv(file_patterns, model)
     
     #print time ranges for given file groups: file_pattern, beg time, end time, etc
-    if output:
+    if print_output:
         print('File pattern: UTC time ranges')
         print('------------------------------------------')
         for key in times_dict.keys():
@@ -238,14 +301,23 @@ def File_Times(model, file_dir, output=True):
     return times_dict
 
 def Var_3D(model):
-    '''Return list of model variables that are three-dimensional. Model agnostic.'''
+    '''Return list of model variables that are three-dimensional. Model agnostic.
+    
+    Input: model: A string or integer indicating the desired model.
+    Output: A list of the standardized variable names associated with the model
+        that have three dimensions (typically time + 2D spatial).
+    '''
     
     #choose the model-specific function to retrieve the 3D variable list
     variable_dict = Model_Variables(model, return_dict=True)    
     return [value[0] for key, value in variable_dict.items() if len(value[4])==3]
 
 def Var_ilev(model):
-    '''Return list of possible ilev coordinate names for model given.'''
+    '''Return list of possible ilev coordinate names for model given.
+    
+    Input: model: A string or integer indicating the desired model.
+    Output: A list of the pressure level coordinate names associated with the model.
+    '''
 
     variable_dict = Model_Variables(model, return_dict=True)   
     ilev_list = list(unique([value[4][-1] for key, value in variable_dict.items() \
@@ -253,13 +325,28 @@ def Var_ilev(model):
     return ilev_list
 
 def Var_units(model, variable_list):
-    '''Returns dictionary of key, value = varname, units.'''
+    '''Determines the proper units for the given variables.
+    
+    Inputs: 
+        model: A string or integer indicating the desired model.
+        variable_list: A list of strings for the desired standardized variable 
+            names.
+    Output: A dictionary of the standardized variable names as keys with the 
+        corresponding units as the values.
+    '''
 
     variable_dict = Model_Variables(model, return_dict=True)  
     return {key:value[-1] for key, value in variable_dict.items() if key in variable_list}    
 
 def convert_variablenames(model, variable_list):
-    '''Given list of integers for variable names, convert to names for given model.'''
+    '''Given list of integers for variable names, convert to names for given model.
+    
+    Inputs:
+        model: A string or integer indicating the desired model.
+        variable_list: A list of strings or integers for the desired 
+            standardized variable names.
+    Output: A list of the desired standardized variable names.
+    '''
 
     if isinstance(variable_list[0], int):
         variable_dict = Model_Variables(model, return_dict=True)          
@@ -269,7 +356,15 @@ def convert_variablenames(model, variable_list):
     return variable_list
     
 def convert_coordnames(coord_type, coord_grid):
-    '''convert integers to strings for coordinate names and grid types.'''
+    '''Convert integers to strings for coordinate names and grid types.
+    
+    Inputs:
+        coord_type: An integer or string corresponding to the desired coordinate 
+            system.
+        coord_grid: An integer or string corresponding to the desired coordinate
+            grid type (e.g. 0 for cartesian or 1 for spherical.)
+    Outputs: Two strings representing the coordinate system and grid type.
+    '''
     
     if isinstance(coord_type, int) and isinstance(coord_grid, int):
         spacepy_dict = {0:'GDZ',1:'GEO',2:'GSM',3:'GSE',4:'SM',5:'GEI',6:'MAG',
@@ -282,7 +377,16 @@ def convert_coordnames(coord_type, coord_grid):
     return coord_type, coord_grid
     
 def coord_units(coord_type, coord_grid):
-    '''return proper units given coordinate system'''
+    '''Determines the proper coordinate units given the coordinate system and type.
+    
+    Inputs:
+        coord_type: An integer or string corresponding to the desired coordinate 
+            system.
+        coord_grid: An integer or string corresponding to the desired coordinate
+            grid type (e.g. 0 for cartesian or 1 for spherical.)
+    Outputs: A dictionary with key, value pairs giving the proper units for each
+        coordinate grid component (e.g. c1, c2, c3 in R_E for cartesian).    
+    '''
     
     if coord_grid=='car':
         if coord_type in ['GDZ','SPH','RLL']:
@@ -296,7 +400,18 @@ def coord_units(coord_type, coord_grid):
             return {'utc_time':'s','net_idx':'','c1':'deg','c2':'deg','c3':'R_E'}
         
 def coord_names(coord_type, coord_grid):
-    '''given type and grid of coordinates, return dicitonary of coordinate names.'''
+    '''Determines the proper coordinate component names for a given coordinate
+    system and grid type.
+    
+    Inputs:
+        coord_type: An integer or string corresponding to the desired coordinate 
+            system.
+        coord_grid: An integer or string corresponding to the desired coordinate
+            grid type (e.g. 0 for cartesian or 1 for spherical.)
+    Outputs: A dictionary with key, value pairs giving the proper coordinate
+        component names for each coordinate grid component (e.g. c1, c2, c3 
+        associated with Longitude, Latitude, and Radius for most spherical systems).    
+    '''
     
     if coord_grid=='car':
         return {'c1':'X_'+coord_type, 'c2':'Y_'+coord_type, 'c3':'Z_'+coord_type}
