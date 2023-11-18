818f22b28042a1bd19f97f6d68fba53adc4f289e
Alexander Drozdov (Sasha) <adrozdov@ucla.edu>
2023-11-17 19:09:56 -0800

    Kamodo object can be created from VERB dataset

--- kamodo_ccmc/readers/verb3d_4D.py
+++ kamodo_ccmc/readers/verb3d_4D.py
@@ -6,28 +6,31 @@
 #                            'Coordinate grid', [Coordinate list], 'Units'],
 
 model_varnames = {'PSD': ['PSD_{lea}', 'Phase Space Density in (L, E, A)', 0, 'LEA',
-                            'car', ['time', 'L', 'Energy', 'Alpha'], '1/(s*cm**2*keV*sr*MeV**2)'],
+                          'car', ['time', 'L', 'Energy', 'Alpha'], '1/(s*cm**2*keV*sr*MeV**2)'],
                   'PSD_2': ['PSD_{lmk}', 'Phase Space Density in (L, \\mu, K)', 0, 'LEA',
                             'car', ['time', 'L', 'Mu', 'K'], '1/(s*cm**2*keV*sr*MeV**2)'],
                   'L': ['L', 'L-shell', 1, 'LEA',
                         'car', ['L', 'Energy', 'Alpha'], ''],
                   'L_2': ['L', 'L-shell', 1, 'LEA',
-                        'car', ['L', 'Mu', 'K'], ''],
-                  'Energy': ['E', 'Energy', 1, 'LEA',
-                        'car', ['L', 'Energy', 'Alpha'], 'MeV'],
+                          'car', ['L', 'Mu', 'K'], ''],
+                  'E': ['E', 'Energy', 1, 'LEA',
+                             'car', ['L', 'Energy', 'Alpha'], 'MeV'],
                   'Alpha': ['Alpha', 'Pitch angle', 1, 'LEA',
-                        'car', ['L', 'Energy', 'Alpha'], 'deg'],
+                            'car', ['L', 'Energy', 'Alpha'], 'deg'],
                   'Mu': ['Mu', '1st adiabatic invariant \\mu', 1, 'LMK',
                          'car', ['L', 'Mu', 'K'], 'MeV/G'],
                   'K': ['K', '2dn adiabatic invariant K', 1, 'LMK',
                         'car', ['L', 'Mu', 'K'], 'GR_E**1/2']
                   }
 
+
 def MODEL():
     from kamodo import Kamodo
     import numpy as np
+    from numpy import array, unique, NaN, append, transpose, where
     import os
     import kamodo_ccmc.readers.reader_utilities as RU
+    from time import perf_counter
 
     # main class
     class MODEL(Kamodo):
@@ -70,18 +73,203 @@ def MODEL():
         Notes:
             -
         '''
+
         def __init__(self, file_dir, variables_requested=[],
                      filetime=False, verbose=False, gridded_int=True,
                      printfiles=False, **kwargs):
             super(MODEL, self).__init__()
             self.modelname = 'VERB-3D'
+            t0 = perf_counter()
+
+            # For debug purpose
+            FORCE_LOAD = False
 
             # first, check for file list, create if DNE
             list_file = file_dir + self.modelname + '_list.txt'
             time_file = file_dir + self.modelname + '_times.txt'
             self.times, self.pattern_files = {}, {}
-            if not RU._isfile(list_file) or not RU._isfile(time_file) or 1:
+            if FORCE_LOAD or not RU._isfile(list_file) or not RU._isfile(time_file):
                 from kamodo_ccmc.readers.verb3d_tocdf import convert_all
                 self.conversion_test = convert_all(file_dir)
+
+            # read in data and time grids from file list
+            self.times, self.pattern_files, self.filedate, self.filename = \
+                RU.read_timelist(time_file, list_file)
+
+            # return time info
+            if filetime:
+                return
+
+            # perform initial check on variables_requested list
+            if len(variables_requested) > 0 and variables_requested != 'all':
+                test_list = [value[0] for key, value in model_varnames.items()]
+                err_list = [item for item in variables_requested if item not in test_list]
+                if len(err_list) > 0:
+                    print('Variable name(s) not recognized:', err_list)
+                for item in err_list:
+                    variables_requested.remove(item)
+                if len(variables_requested) == 0:
+                    return
+
+            # store variables
+            self.missing_value = NaN
+            self.varfiles = {}  # store which variable came from which file
+            self.gvarfiles = {}  # store file variable name similarly
+            self.gvarfiles_full = {}  # All {patterns: [variables], ...}
+            self.err_list = []
+            self.var_dict = {}
+
+            self.patterns = list(self.pattern_files.keys())
+
+            for p in self.patterns:
+                pattern_files = self.pattern_files[p]
+
+                # Kamodo RU.create_timelist discards file path for no reason
+                pattern_files = [os.path.join(file_dir, 'Output', file) for file in pattern_files]
+
+                with RU.Dataset(pattern_files[0], 'r') as cdf_data:
+                    # check var_list for variables not possible in this file set
+                    self.gvarfiles_full.update({p: [k for k in cdf_data.variables.keys()]})
+
+                    # TODO: Check this condition
+                    if len(variables_requested) > 0 and \
+                            variables_requested != 'all':
+                        gvar_list = [key for key in model_varnames.keys()
+                                     if key in cdf_data.variables.keys() and
+                                     model_varnames[key][0] in variables_requested]
+                        if len(gvar_list) != len(variables_requested):
+                            err_list = [value[0] for key, value in
+                                        model_varnames.items()
+                                        if key not in cdf_data.variables.keys() and
+                                        value[0] in variables_requested]
+                            self.err_list.extend(err_list)  # add to master list
+                    else:
+                        gvar_list = [key for key in model_varnames.keys()
+                                     if key in cdf_data.variables.keys()]
+
+                # store which file these variables came from
+                self.varfiles[p] = [model_varnames[key][0] for key in gvar_list]
+                self.gvarfiles[p] = gvar_list
+
+                # print message if variables not found
+                if len(self.err_list) > 0:
+                    print('Some requested variables are not available: ',
+                          self.err_list)
+
+            # collect all possible variables in set of files and return
+            if variables_requested == 'all':
+                for p in self.patterns:
+                    var_list = self.varfiles[p]
+
+                    self.var_dict.update({value[0]: value[0:] for key, value in
+                                          model_varnames.items() if value[0] in
+                                          var_list})
+                return
+
+            # option to print files
+            if printfiles:
+                print(f'{len(self.filename)} Files:')
+                files = self.filename.split(',')
+                for f in files:
+                    print(f)
+
+            # initialize storage structure
+            self.variables = {}
+            for p in self.patterns:
+                variables = {model_varnames[gvar][0]: {
+                    'units': model_varnames[gvar][-1],
+                    'data': p} for gvar in self.gvarfiles[p]}
+                self.variables.update(variables)
+
+            # Load required grid variables
+            self.load_grid(file_dir)
+
+            # register interpolators for each variable
+            t_reg = perf_counter()
+            # store original list b/c gridded interpolators change keys list
+            varname_list = [key for key in self.variables.keys()]
+            for varname in varname_list:
+                self.register_variable(varname, gridded_int, file_dir)
+
+            if verbose:
+                print(f'Took {perf_counter() - t_reg:.5f}s to register ' +
+                      f'{len(varname_list)} variables.')
+            if verbose:
+                print(f'Took a total of {perf_counter() - t0:.5f}s to kamodofy' +
+                      f' {len(varname_list)} variables.')
+
+        def register_variable(self, varname, gridded_int, file_dir):
+            """Registers an interpolator with proper signature"""
+
+            # coord_dict = {'time': {'units': 'hr', 'data': time},
+            #               'L': {'units': '', 'data': L},
+            #               'mu': {'units': 'MeV/G', 'data': mu},
+            #               'K': {'units': 'R_E/G**0.5', 'data': K}}
+            # var_dict = {'PSD': {'units': '(c/cm/MeV)**3', 'data': PSD}}
+
+            grid_variabels = model_varnames[varname][5]
+            coord_dict = {}
+
+            # Handle time
+            key = self.variables[varname]['data']
+            if 'time' in grid_variabels:
+                coord_dict = {'time': {'units': 'hr',
+                                       'data': self.times[key]['all']}}
+
+            for coord in grid_variabels:
+                if coord != 'time':
+                    coord_dict.update({coord: {'units': model_varnames[coord][6], 'data': getattr(self, coord)}})
+
+            variable_name = model_varnames[varname][0]
+            coord_str = ''
+
+            def func(i, fi):  # i = file#, fi = slice#
+                pattern_files = self.pattern_files[key]
+
+                # Kamodo RU.create_timelist discards file path for no reason
+                pattern_files = [os.path.join(file_dir, 'Output', file) for file in pattern_files]
+
+                with RU.Dataset(self.pattern_files[key][i]) as cdf_data:
+                    data = array(cdf_data.variables[varname][fi])
+                return data
+
+            # functionalize the 3D or 4D dataset, series of time slices
+            self = RU.Functionalize_Dataset(
+                self, coord_dict, varname, self.variables[varname],
+                gridded_int, coord_str, interp_flag=3, func=func,
+                times_dict=self.times[key])
+            return
+
+        def load_grid(self, file_dir):
+            """Load grid data according to the requested variables"""
+
+            # Determine the variables
+            variables = [v for gvars in self.gvarfiles.values() for v in gvars]
+
+            # Determine the grid variables
+            grid_variables = set(item for v in variables for item in model_varnames[v][5])
+
+            # Exclude time
+            if 'time' in grid_variables:
+                grid_variables.remove('time')
+
+            # Find what patterns to load
+            patterns_to_load = set()
+            for key, value in self.gvarfiles_full.items():
+                if any(item in grid_variables for item in value):
+                    patterns_to_load.add(key)
+
+            # Load grids
+            for p in patterns_to_load:
+                pattern_files = self.pattern_files[p]
+
+                # Kamodo RU.create_timelist discards file path for no reason
+                pattern_files = [os.path.join(file_dir, 'Output', file) for file in pattern_files]
+
+                with RU.Dataset(pattern_files[0], 'r') as cdf_data:
+                    for var in self.gvarfiles_full[p]:
+                        if var in grid_variables:
+                            # TODO: consider adding leading _ to avoid naming problems
+                            setattr(self, var, array(cdf_data.variables[var]))
 
     return MODEL
--- kamodo_ccmc/readers/verb3d_tocdf.py
+++ kamodo_ccmc/readers/verb3d_tocdf.py
@@ -7,35 +7,69 @@ import os
 import pyverbplt
 import kamodo_ccmc.readers.reader_utilities as RU
 from datetime import datetime
+import re
+
+def get_start_date(file_dir):
+    '''Return the start date based on the information from DatabaseInfo1'''
+
+    # Default date_start
+    date_start = datetime(1, 1, 1)
+
+    # Determine if there is a file that contains userinput
+    database_filename = os.path.join(file_dir, 'DatabaseInfo1')
+    if RU._isfile(database_filename):
+
+        # Define the regex pattern for the date and time
+        pattern = r'(\d{4}/\d{2}/\d{2} \d{2}:\d{2})\s+# start_time'
+
+        with open(database_filename, 'r') as file:
+            for line in file:
+                match = re.search(pattern, line)
+                if match:
+                    date_str = match.group(1)  # Return only the date part
+                    date_start = datetime.strptime(date_str, '%Y/%m/%d %H:%M')
+
+    return date_start
+
 
 def convert_all(file_dir):
     '''Converting all plt files to netCDF4.'''
 
-    # TODO: add check for files existence
-
     perp_grid_filename = os.path.join(file_dir, 'Output', 'perp_grid.plt')
+    psd_filename = os.path.join(file_dir, 'Output', 'OutPSD.dat')
+
+    # check for files existence
+    if not RU._isfile(perp_grid_filename) or not RU._isfile(psd_filename):
+        return False
+
+    # TODO: check if the files needs to be updated
+
+    # TODO: Add radial grid and radial PSD
+
+    # Get the start date
+    start_date = get_start_date(file_dir)
 
     # Work with grid
     grid = pyverbplt.load_plt(perp_grid_filename, squeeze=True)
     data = {'L': grid[0]['arr'],
             'E': grid[1]['arr'],
-            'alpha': grid[2]['arr'],
+            'Alpha': grid[2]['arr'],
             'pc': grid[3]['arr']}
 
-    cdf_filename = os.path.join(file_dir, 'Output', 'rad_grid.nc')
+    cdf_filename = os.path.join(file_dir, 'Output', 'perp_grid.nc')
     var_shape = grid[0]['arr'].shape
+    grid_file = [cdf_filename]
     with Dataset(cdf_filename, 'w', format='NETCDF4') as ncfile:
         # Create dimensions
         ncfile.createDimension('L', var_shape[0])
         ncfile.createDimension('E', var_shape[1])
-        ncfile.createDimension('alpha', var_shape[2])
+        ncfile.createDimension('Alpha', var_shape[2])
 
         for key, var in data.items():
-            data_var = ncfile.createVariable(key, np.float32, ('L', 'E', 'alpha'))
+            data_var = ncfile.createVariable(key, np.float32, ('L', 'E', 'Alpha'))
             data_var[:] = var
 
     # Work with PSD
-    psd_filename = os.path.join(file_dir, 'Output', 'OutPSD.dat')
     psd = pyverbplt.load_plt(psd_filename)
 
     time = np.array([np.float32(t) for t in psd['zone']])
@@ -50,35 +84,34 @@ def convert_all(file_dir):
             ncfile.createDimension('time', 1)
             ncfile.createDimension('L', psd_size[1])
             ncfile.createDimension('E', psd_size[2])
-            ncfile.createDimension('alpha', psd_size[3])
+            ncfile.createDimension('Alpha', psd_size[3])
 
-            psd_var = ncfile.createVariable('PSD', np.float32, ('L', 'E', 'alpha'))
+            psd_var = ncfile.createVariable('PSD', np.float32, ('L', 'E', 'Alpha'))
             psd_var[:] = psd['arr'][t, :, :, :]
 
+            # Time is number of days from zero - start of the simulation, directly from zone
             time_var = ncfile.createVariable('time', np.float32, ('time'))
             time_var[:] = time[t]
-
-
 
     modelname = 'VERB-3D'
     # List of files for Kamodo reader
     list_file = file_dir + modelname + '_list.txt'
     time_file = file_dir + modelname + '_times.txt'
 
-    pattern_files = {'OutPSD': nc_files}
-    times_list = list(time)
+    pattern_files = {'OutPSD': nc_files, 'perp_grid': grid_file}
+    times_list = list(time * 24)  # Convert to number of hours
     times_end = times_list
     if len(times_list) > 1:
         times_end = times_list[1:]
         times_end.append(times_end[-1] + times_list[-1] - times_list[-2])
 
-    # TODO: Change to
     # All times are stored as the number of hours since midnight of the
     # first file of all the files in the given directory.
-    times = {'OutPSD': {'start': times_list, 'end': times_end, 'all': times_list}}
+    times = {'OutPSD': {'start': times_list, 'end': times_end, 'all': times_list},
+             'perp_grid': {'start': [times_list[0]], 'end': [times_end[-1]], 'all': [times_list[0]]}}
 
     RU.create_timelist(list_file, time_file, modelname,
                        times, pattern_files,
-                       datetime(1, 1, 1))
+                       start_date)
 
     return True
